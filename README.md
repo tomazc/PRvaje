2f54500ec2ed6534018eebe9b95c9471809bad06
Tomaž Curk <tomazc@gmail.com>
2019-02-24 22:40:17 +0100

    setup: requirements za mybinder

--- README.md
+++ README.md
@@ -15,5 +15,5 @@ Rok Gomišček, Martin Stražar, Tomaž Curk
 University of Ljubljana, Faculty of Computer and Information Science
 
 You can run this collection in binder:
-[![Binder](https://mybinder.org/badge_logo.svg)](https://mybinder.org/v2/gh/tomazc/PRvaje/master?filepath=web-sl)
+[![Binder](https://mybinder.org/badge_logo.svg)](https://mybinder.org/v2/gh/tomazc/PRvaje/master?filepath=web-en)
 
--- skripte/pip_install.sh
+++ skripte/pip_install.sh
@@ -3,12 +3,12 @@
 # Essentials
 pip install --upgrade numpy
 pip install --upgrade scipy
+pip install --upgrade Pillow
 pip install --upgrade matplotlib
-pip install --upgrade GPy`
+pip install --upgrade GPy
 
 # Orange and requirements
-pip install --upgrade chardet
-pip install --upgrade Orange
+pip install --upgrade Orange3
 
 # Scikit-learn
 pip install --upgrade sklearn
@@ -17,6 +17,7 @@ pip install --upgrade sklearn
 pip install --upgrade terminado
 pip install --upgrade functools32
 pip install --upgrade jupyter
+pip install --upgrade jupyter_contrib_nbextensions
 
 # Test installations
 python -c "import Orange"
