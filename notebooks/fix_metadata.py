0984a483607b01b955701b2848c6b5520c53c937
Tomaž Curk <tomazc@gmail.com>
2020-02-28 18:07:42 +0100

    naloge: manjsi popravki

--- notebooks/fix_metadata.py
+++ notebooks/fix_metadata.py
@@ -1,12 +1,15 @@
 import sys
 import nbformat
 
+print(sys.argv)
 fn = sys.argv[1]
+lang = sys.argv[2]
+print(fn, lang)
 
 with open(fn, 'rt') as fnb:
     notebook = nbformat.read(fnb, as_version=4)
 
-notebook['metadata'].setdefault('nbTranslate', {})['displayLangs'] = ["sl"]
+notebook['metadata'].setdefault('nbTranslate', {})['displayLangs'] = [lang]
 notebook['metadata'].pop('celltoolbar', None)
 
 with open(fn, 'wt') as fnb:
